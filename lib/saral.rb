b28d61fda263a3037c3396175a9480db30c67335
Javier Donaire <jdonaire@gmail.com>
2014-01-19 21:23:02 +0100

    add Application class

--- lib/saral.rb
+++ lib/saral.rb
@@ -1,5 +1,11 @@
 require "saral/version"
 
 module Saral
-  # Your code goes here...
+
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ["Hello from the Saral framework!"]]
+    end
+  end
+
 end
--- lib/saral/version.rb
+++ lib/saral/version.rb
@@ -1,3 +1,3 @@
 module Saral
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
